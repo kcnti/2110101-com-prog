c8cad21185ef31d7f5d3589a90def5e389d89f8c
kcnti <kantinun99998@gmail.com>
2023-01-19 10:24:13 +0700

    refactor: solved

--- strlist/01.py
+++ strlist/01.py
@@ -1,8 +1,5 @@
 n = input()
-tmp = 0
 ntmp = 13
-for i in n:
-    tmp += int(i) * ntmp
-    ntmp-=1
+tmp = sum([int(n[13-i])*i for i in range(ntmp, ntmp-len(n), -1)])
 n12 = (11-(tmp)%11)%10
 print("{} {} {} {} {}".format(n[0], n[1:5], n[5:10], n[10:], n12))
