08d0ffe220a96dac0f7d65b17def945215c440ec
kcnti <kantinun99998@gmail.com>
2023-01-26 20:03:55 +0700

    feat: weehewwww

--- grader/getPDF.py
+++ grader/getPDF.py
@@ -1,16 +1,30 @@
 import requests
 import sys
 import os
+import re
 
 from dotenv import load_dotenv
 load_dotenv()
 
-
-
 COOKIE = os.getenv('COOKIE')
 
-url = sys.argv[1]
+select = sys.argv[1]
 
-r = requests.get(url, headers={'Cookie': COOKIE})
+URL = "https://2110101.nattee.net/python/main/list"
 
-open(url.split('/')[-1], 'wb').write(r.content)
+r = requests.get(URL, headers={"cookie": COOKIE})
+html = r.text
+
+output = re.findall('<option value="([^"]+)">\[([^"]+)\]\s+(?:.*?\s+)?', html)
+
+
+URL = "https://2110101.nattee.net/python/tasks/download/"
+
+for i in output:
+
+    newURL = f"{URL}/{i[0]}/{i[1]}.pdf"
+
+    if select == i[1][:2]: # 01 02 03 ...
+        r = requests.get(newURL, headers={'Cookie': COOKIE})
+
+        open(i[1]+'.pdf', 'wb').write(r.content)
