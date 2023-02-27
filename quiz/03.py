a6a2b3f71b8daea29ed2bef4e8ebfe4e93b30184
kcnti <kantinun99998@gmail.com>
2023-02-27 19:27:30 +0700

    done

--- quiz/03.py
+++ quiz/03.py
@@ -1,1 +1,22 @@
-# flowchart ไรไม่รู้ลืมละ but easy
+a, b, c, d = [int(i) for i in input().split()]
+
+if d > a:
+    n = 0
+    while a < d:
+        n = n + 1
+        a = a + abs(b)
+        d = d - abs(c)
+    print(n, a, d)
+    
+else:
+    a, d = d, a
+    if a > b:
+        a, b = b, a
+    if c > d:
+        c, d = d, c
+    if b == c:
+        print(a, b, d)
+    elif b > c:
+        print((b+c)/2)
+    else:
+        print(a,b,c,d)
